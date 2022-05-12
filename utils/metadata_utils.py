29d587e6c120919d5d2d06c21b7c964648ec8473
sstulajter <stefan.s.stulajter@gmail.com>
2022-05-12 15:28:47 +0200

    Testing.

--- utils/metadata_utils.py
+++ utils/metadata_utils.py
@@ -1,40 +1,43 @@
+from math import ceil
+import MySQLdb
 import os.path
 import yaml
-import MySQLdb
 
 
-def get_tables(tables_file):
-    # Reading tables.yaml, extracting tables to be split
-    sql_table_names = {
-        "backend.accounts_payment": "shapeupclub.accounts_payment",
-        "backend.dietsettings": "shapeupclub.diary_dietsetting",
-        "backend.food_food": "shapeupclub.tblfood",
-        "backend.food_mealitem": "shapeupclub.tblmealitem",
-        "backend.tbluser": "shapeupclub.tbluser",
-    }
+def filter_dict_by_list(_dict, _list):
+    intersection = sorted(set(_dict) & set(_list))
+    return {x: _dict[x] for x in intersection}
 
+
+# Reading tables.yaml, and metadata.yaml extracting tables to be split
+def get_tables_metadata(tables_file, metadata_file):
+    # Reading tables.yaml file
     with open(tables_file, "r") as file:
         try:
-            parsed_yaml = yaml.safe_load(file)
+            tables_yaml = yaml.safe_load(file)
         except yaml.YAMLError as exc:
             print(exc)
 
-    tables = {}
-    for group, table_names in parsed_yaml.items():
-        if (group.endswith("_split")):
-            for table_name in table_names:
-                sql_db_table = sql_table_names.get(table_name)
-                if (sql_db_table is not None):
-                    sql_db_table_split = sql_db_table.split(".")
-                    sql_db = sql_db_table_split[0]
-                    sql_table = sql_db_table_split[1]
-                    tables[table_name] = {"sql_db": sql_db, "sql_table": sql_table}
+    # Reading metadata.yaml file
+    with open(metadata_file, "r") as file:
+        try:
+            metadata_yaml = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-    print(tables)
-    return tables
+    # Extracting matched values
+    tables_metadata = {}
+    for metadata_table_group, metadata_tables in metadata_yaml.items():
+        # Matching group
+        if metadata_table_group in tables_yaml:
+            # Matching items (tables)
+            tables_metadata[metadata_table_group] =\
+                filter_dict_by_list(metadata_tables, tables_yaml[metadata_table_group])
+
+    return tables_metadata
 
 
-def get_metadata(tables):
+def get_sql_metadata(metadata):
     # SQL connection setup
     connection = MySQLdb.connect(
             **{
@@ -48,48 +51,83 @@ def get_metadata(tables):
         )
 
     # Select data
-    for table, metadata in tables.items():
-        print(f"""Getting metadata for {metadata.get("sql_db")}.{metadata.get("sql_table")} table.""")
+    for metadata_table_group, metadata_tables in metadata.items():
+        for metadata_table in metadata_tables:
+            sql_db = metadata_tables[metadata_table].get("sql_db")
+            sql_table = metadata_tables[metadata_table].get("sql_table")
+            print(f"""Getting SQL metadata for {sql_db}.{sql_table} table.""")
 
-        sql = f"""
-        SELECT count, size_mb
-        FROM (
-            SELECT COUNT(*) AS count
-            FROM {metadata.get("sql_db")}.{metadata.get("sql_table")}
-        ) AS counts
-        JOIN (
-            SELECT ROUND(((data_length + index_length) / 1024 / 1024), 2) AS size_mb
-            FROM information_schema.TABLES
-            WHERE table_schema = "{metadata.get("sql_db")}"
-            AND table_name = "{metadata.get("sql_table")}"
-        ) AS sizes
-        """
+            sql = f"""
+            SELECT count, size_mb
+            FROM (
+                SELECT COUNT(*) AS count
+                FROM {sql_db}.{sql_table}
+            ) AS counts
+            JOIN (
+                SELECT ROUND(((data_length + index_length) / 1024 / 1024), 2) AS size_mb
+                FROM information_schema.TABLES
+                WHERE table_schema = "{sql_db}"
+                AND table_name = "{sql_table}"
+            ) AS sizes
+            """
 
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        cursor.close()
-        print(f"count = {int(result[0])}, size = {float(result[1])} MB")
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            cursor.close()
+            print(f"count = {int(result[0])}, size = {float(result[1])} MB")
 
-        tables[table]["count"] = int(result[0])
-        tables[table]["size_mb"] = float(result[1])
+            metadata[metadata_table_group][metadata_table]["count"] = int(result[0])
+            metadata[metadata_table_group][metadata_table]["size_mb"] = float(result[1])
 
     connection.close()
 
-    print(tables)
-    return tables
+    return metadata
 
 
-def write_metadata(tables_split_file, metadata):
+def enrich_metadata(metadata):
+    # Calculate desired split sizes
+    # TODO: Extract into YAML file?
+    split_data = {
+        "backend_split": {
+            "measured_throughput_mb_m": 160,
+            "target_split_run_time_m": 30,
+            "default_split": 5
+        }
+    }
+
+    for metadata_table_group, metadata_tables in metadata.items():
+        # Matching group
+        if metadata_table_group in split_data:
+            for metadata_table in metadata_tables:
+                # Calculating the split size based on the measurements and current table size
+                sql_db = metadata_tables[metadata_table].get("sql_db")
+                sql_table = metadata_tables[metadata_table].get("sql_table")
+                size_mb = metadata_tables[metadata_table].get("size_mb")
+                throughput = split_data[metadata_table_group].get("measured_throughput_mb_m")
+                target_run_time = split_data[metadata_table_group].get("target_split_run_time_m")
+                default_split = split_data[metadata_table_group].get("default_split")
+                split = max(default_split, int(ceil(size_mb / throughput / target_run_time)))
+
+                print(f"""Split size for {sql_db}.{sql_table} ({size_mb} MB) = {split} (default {default_split}).""")
+
+                metadata[metadata_table_group][metadata_table]["default_split"] = default_split
+                metadata[metadata_table_group][metadata_table]["split"] = split
+
+    return metadata
+
+
+def write_tables_metadata(tables_metadata_file, metadata):
     # Writing values into tables_counts.yaml
-    with open(tables_split_file, "w") as file:
+    with open(tables_metadata_file, "w") as file:
         documents = yaml.dump(metadata, file, indent = 4, sort_keys = False)
 
 
 if __name__ == '__main__':
     path = "dags/conf"
     tf = f"{path}/tables.yaml"
-    tsf = f"{path}/tables_split.yaml"
+    mf = f"{path}/metadata.yaml"
+    tmf = f"{path}/tables_metadata.yaml"
 
     if os.path.isfile("env/env.py"):
         print("Env file does exist, pulling the environment variables locally.")
@@ -98,6 +136,7 @@ if __name__ == '__main__':
     else:
         print("Env file does not exist, pulling the environment variables from server.")
 
-    tbls = get_tables(tf)
-    mtd = get_metadata(tbls)
-    #write_metadata(tsf, mtd)
+    mtd = get_tables_metadata(tf, mf)
+    mtd = get_sql_metadata(mtd)
+    mtd = enrich_metadata(mtd)
+    write_tables_metadata(tmf, mtd)
